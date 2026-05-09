05354bea82d11ce28559ee58737b3f1f35251e74
mekeirish <mmm1747lb@gmail.com>
2026-05-09 09:42:28 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE = 'v1';
+const assets = ['./', './index.html', './manifest.json', './icon.png'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(assets))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
